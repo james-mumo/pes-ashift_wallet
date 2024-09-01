8c62187a8c1ffdfd007f334be0fcf8f55923f857
wildcard98 <98mumo@gmail.com>
2024-09-01 12:00:25 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,45 @@
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET;
 const rootRouter = require('./routes/index.js');
 
 const app = express();
+const PORT = process.env.PORT || 3500;
 
-const PORT = process.env.PORT || 3500
+// Specify allowed origins
+const allowedOrigins = ['https://pesa-shift-wallet.vercel.app'];
 
-app.use(cors());
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+};
+
+// Use CORS middleware with options
+app.use(cors(corsOptions));
 app.use(express.json());
-
 
 app.use('/api/v1', rootRouter);
 
-
-
-app.use( (req, res) => {
-    const err =  new Error( 'File not found');
+app.use((req, res) => {
+    const err = new Error('File not found');
     err.status = 404;
-    throw err
-})
+    throw err;
+});
 
 app.use((err, req, res, next) => {  
     res.status(err.status || 500).json({
-        message: err.message || "Oops! Something went wrong"
-    })
-})
-app.listen(PORT, ()=>{
-    console.log(`Server running on PORT ${PORT} `)
-})
+        message: err.message || "Oops! Something went wrong",
+    });
+});
 
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
